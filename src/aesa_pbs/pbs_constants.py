5554d7ca58bb0ae400ce7faffc451a6e3b954509
vtulus <47393746+vtulus@users.noreply.github.com>
2023-01-27 11:45:52 +0100

    Add `pbs_constants.py`

---
+++ src/aesa_pbs/pbs_constants.py
@@ -0,0 +1,5 @@
+# - add dict with current scores of PBs as per Steffen et al.?
+# - add dict with SOS of PBs as per Steffen et al.?
+# can get names of categories from aesa_pbs.py?
+# like: from aesa_pbs import add_aesa_pbs
+
